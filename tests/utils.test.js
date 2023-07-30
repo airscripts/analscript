5ffcabc2377ea6e461c35a475c2498b8c510eeba
Airscript <francesco@airscript.it>
2023-07-30 16:09:18 +0200

    test: add spy on process exit

--- tests/utils.test.js
+++ tests/utils.test.js
@@ -5,12 +5,19 @@ import {
   describe,
 } from 'vitest';
 
-import { graceful } from '../lib/utils';
+import { graceful } from '../lib/utils.js';
+import { EMPTY_STRING_INPUT } from './seeds.js';
 
 describe('Graceful', () => {
   test('Execute graceful', () => {
     const fn = vi.fn();
     graceful(fn);
     expect(fn).toBeCalled();
+  });
+
+  test('Trigger graceful exit', () => {
+    const processExit = vi.spyOn(process, 'exit').mockImplementation(() => {});
+    graceful(EMPTY_STRING_INPUT);
+    expect(processExit).toHaveBeenCalled();
   });
 });
