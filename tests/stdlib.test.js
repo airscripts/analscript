8c133f4c332772889001bc9e80fb7ef5f090cb16
mateonunez <mateonunez95@gmail.com>
2023-07-30 11:54:44 +0200

    test(stdlib): full coverage
    Signed-off-by: mateonunez <mateonunez95@gmail.com>

--- tests/stdlib.test.js
+++ tests/stdlib.test.js
@@ -1,4 +1,4 @@
-import { test, expect } from 'vitest';
+import { test, expect, describe } from 'vitest';
 
 import {
   ANALLIFY_INPUT,
@@ -16,19 +16,69 @@ import {
   run,
   anallify,
   stringify,
+  compile,
 } from '../lib/stdlib.js';
+import { ERROR } from '../lib/dictionary.js';
 
-test('Encode string to anal', () => {
-  expect(anallify(ANALLIFY_INPUT)).toBe(ANALLIFY_CORRECT_OUTPUT);
-  expect(anallify(ANALLIFY_INPUT)).not.toBe(ANALLIFY_WRONG_OUTPUT);
+describe('Anallify', () => {
+  test('Encode string to anal', () => {
+    expect(anallify(ANALLIFY_INPUT)).toBe(ANALLIFY_CORRECT_OUTPUT);
+    expect(anallify(ANALLIFY_INPUT)).not.toBe(ANALLIFY_WRONG_OUTPUT);
+  });
+
+  test('Throw error if argument is not a string', () => {
+    expect(() => anallify(1)).toThrowError(Error(ERROR.notString));
+  });
+
+  test('Throw error if argument is missing', () => {
+    expect(() => anallify('')).toThrowError(Error(ERROR.missingArgument));
+  });
 });
 
-test('Decode anal to string', () => {
-  expect(stringify(STRINGIFY_INPUT)).toBe(STRINGIFY_CORRECT_OUTPUT);
-  expect(stringify(STRINGIFY_INPUT)).not.toBe(STRINGIFY_WRONG_OUTPUT);
+describe('Stringify', () => {
+  test('Decode anal to string', () => {
+    expect(stringify(STRINGIFY_INPUT)).toBe(STRINGIFY_CORRECT_OUTPUT);
+    expect(stringify(STRINGIFY_INPUT)).not.toBe(STRINGIFY_WRONG_OUTPUT);
+  });
+
+  test('Decode with correct chars', () => {
+    const ANAL_CHARACTERS = 'anal';
+    const charCodeA = STRINGIFY_INPUT.length;
+    const charCodeB = STRINGIFY_INPUT.length * 2;
+
+    const anal = `${ANAL_CHARACTERS.repeat(charCodeA)} ${ANAL_CHARACTERS.repeat(charCodeB)}`;
+    const result = stringify(anal);
+
+    expect(result).toBe(String.fromCharCode(charCodeA) + String.fromCharCode(charCodeB));
+  });
+
+  test('Throw error if argument is not a string', () => {
+    expect(() => stringify(1)).toThrowError(Error(ERROR.notString));
+  });
+
+  test('Throw error if argument is missing', () => {
+    expect(() => stringify('')).toThrowError(Error(ERROR.missingArgument));
+  });
 });
 
-test('Run .anal file', () => {
-  expect(run(ANAL_FILE_LOCATION)).toBe(RUN_CORRECT_OUTPUT);
-  expect(run(ANAL_FILE_LOCATION)).not.toBe(RUN_WRONG_OUTPUT);
+describe('Run', () => {
+  test('Run .anal file', () => {
+    expect(run(ANAL_FILE_LOCATION)).toBe(RUN_CORRECT_OUTPUT);
+    expect(run(ANAL_FILE_LOCATION)).not.toBe(RUN_WRONG_OUTPUT);
+  });
+
+  test('Throw error if file is not found', () => {
+    expect(() => run('')).toThrowError(Error(ERROR.fileNotFound));
+  });
+});
+
+describe('Compile', () => {
+  test('Compile .anal file', () => {
+    expect(compile(ANAL_FILE_LOCATION)).toBe(RUN_CORRECT_OUTPUT);
+    expect(compile(ANAL_FILE_LOCATION)).not.toBe(RUN_WRONG_OUTPUT);
+  });
+
+  test('Throw error if file is not found', () => {
+    expect(() => run('')).toThrowError(Error(ERROR.fileNotFound));
+  });
 });
